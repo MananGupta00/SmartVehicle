1f27a9b693e48f03ed9f57fcc1fe9804b0944556
Manan Gupta <54077213+MananGupta00@users.noreply.github.com>
2020-01-21 11:08:30 +0530

    Add files via upload

--- Ir_sih.ino
+++ Ir_sih.ino
@@ -4,17 +4,18 @@ SoftwareSerial mySerial(9, 10);
 const int igRelay=4;
 const int tamperRelay=5;
 const int IRin=6;
-const int FuelIN=A7;
+const int FuelIN=A5;
 //defined pins
+
 bool turned= true;
 bool parkingmode= true;
 int gpsCount=0;
 int oldgpslat=0;
 int oldgpslon=0;
-
+int oldFuel=0;
 
 //registered phone no.
-String phno="+919758183355"; 
+String phno="+918630877372"; 
 
 
 void setup() {
@@ -24,7 +25,7 @@ void setup() {
   pinMode(FuelIN,INPUT);
   Serial.begin(9600); 
 
-  mySerial.begin(9600); 
+  mySerial.begin(9600);
   mySerial.println("AT+CNMI=2,2,0,0,0\r"); // AT Command to receive a live SMS
   delay(100);
 }
@@ -35,11 +36,11 @@ void RecieveMessage()
   String info="",mesg="";
     //To access message phone no.
   if(mySerial.available()!=0)
-    { mySerial.readStringUntil('\n');
+    { Serial.println(mySerial.readStringUntil('\n'));
       info = mySerial.readStringUntil('\n');
       mesg= mySerial.readStringUntil('\n');
       phn= info.substring(7,20);
-
+      Serial.println(phn+"\n"+mesg);
       if(phn==phno)
       { 
       Serial.print("message- ");
@@ -47,7 +48,7 @@ void RecieveMessage()
             mesg= mesg.substring(0,l-1);
       Serial.println(mesg);
 
-     mesg="LOCATE";  //Testing
+    // mesg="LOCATE";  //Testing
       
       if(mesg=="TURNOFF")
       {
@@ -76,8 +77,8 @@ void SendSMS(String msg)
     Serial.print("sending- "+msg);
   mySerial.println("AT+CMGF=1");    
   delay(500);
-  mySerial.println("AT+CMGS=\"+919758183355\"\r"); 
-  mySerial.print(" Hello! I am Bike Security. ");
+  mySerial.println("AT+CMGS=\"+918630877372\"\r"); 
+  mySerial.print("Bike Security: ");
   mySerial.print(msg);
   delay(200);
   mySerial.println((char)26); //the stopping character Ctrl+Z
@@ -95,29 +96,36 @@ void alert(String type){
 void tamperWire(){
    int untemp= digitalRead(tamperRelay);
    delay(2);
-   if(untemp==0)
+   if(untemp==1)
     {
-     alert("Ignition Wire Tampering ALERT");
+      Serial.println("Ignition Wire Tampering");
+     //alert("Ignition Wire Tampering ALERT");
      }
   }
 
 void checkPark(){
    int untemp= digitalRead(tamperRelay);
     delay(2);
-    if(untemp==1)
+    if(untemp==0)
     {
-     parkingmode=true;
+      Serial.println("CHECK DONE");
+      parkingmode=true;
      }
   }
 
 String bluetooth(){
-  String reads="";
-    Serial.readStringUntil('\n');
-    delay(100);
-  if(Serial.available()!=0)
-    reads = Serial.readStringUntil('\n');
-    delay(5);
-    return reads;
+  String c="";
+  delay(100);
+  
+   if(Serial.available()>0)  {
+    c = Serial.readStringUntil('\n');
+   
+   if((c.length())>22)
+    c=c.substring(0, c.indexOf("La",5));
+    Serial.println(c);
+    
+   }
+   return c;
   }
 
 
@@ -125,15 +133,17 @@ void checkfinger(){
   String incoming= bluetooth();
   
   if (incoming=="finger")   
-      ignite();
+      {
+        ignite();
+ }
   else if(incoming=="alert")
-      alert("Unauthentic FingerPrint ALERT");
+     ;// alert("Unauthentic FingerPrint ALERT");
   else if(incoming!="")
       {
-        if(gpsCount==0)
-      checkGPS(incoming); //Checking for location change
-      else
-      gpsCount--;
+     //   if(gpsCount==0)
+     // checkGPS(incoming); //Checking for location change
+     // else
+      //gpsCount--;
       }
   }
 
@@ -183,27 +193,31 @@ void irsensor(){
   }
 
 void ignite(){
- 
+   parkingmode= false;
+  digitalWrite(igRelay ,HIGH);
  Serial.print("Igniting");
- parkingmode=false;
- digitalWrite(igRelay,HIGH);
+
+
  delay(500);
   }
 
 void loop() {
-  RecieveMessage();
+ RecieveMessage();
 
-  if(turned==true)
-  {    
-    if(parkingmode==true){
-      //checkfinger();
+/* if(turned==true)
+  {   
+    if(parkingmode==true)
+    {  Serial.println("turned true park true");
+      digitalWrite(igRelay, LOW);
+      checkfinger();
       irsensor();
-      //tamperWire();
+     // tamperWire();
      }
     else
       checkPark();
   }
   else{
     digitalWrite(igRelay,HIGH);
-    }
+    } 
+    */
 }
